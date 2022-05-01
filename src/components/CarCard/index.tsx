f1c4356a67ccd050a9c81291e5d6b232e7179b71
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-04-30 23:42:20 -0300

    fix

--- src/components/CarCard/index.tsx
+++ src/components/CarCard/index.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
 import { Car } from '../../types/car';
 import { Carousel, Card } from "react-bootstrap";
 import './styles.css';
-import { isNamedExportBindings } from 'typescript';
 
 const carImage = 'https://1.bp.blogspot.com/-m1Jn9m0IAYI/YMEKSZYJmTI/AAAAAAADKic/GL4w5FnKukcJny5mUMormoZj7M_C_8qnACLcBGAsYHQ/s1479/29%2B%25281%2529.jpg';
 const carImage2 = 'https://s1.1zoom.me/big3/575/BMW_Tuning_4-Series_Black_Metallic_576020_3840x2160.jpg';
@@ -30,15 +28,7 @@ function CarCard({ car }: Props) {
         <Card className="m-2 bg-light">
             <Carousel fade interval={null}>
                 {carouselItem}
-                {/* <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={car.imgs[0]}
-                        alt="First slide"
-                    />
-                </Carousel.Item> */}
             </Carousel>
-            {/* <Card.Img variant="top" src={carImage2} /> */}
             <Card.Body>
                 <Card.Title style={{ width: '100%' }} className="d-inline-block text-truncate">{car.assembler} {car.model}</Card.Title>
                 <Card.Text>
@@ -46,28 +36,6 @@ function CarCard({ car }: Props) {
                 </Card.Text>
             </Card.Body>
         </Card>
-        /*<div className={"card-container"}>
-            <div className={"card-body"}>
-                <div className={"row"}>
-                    <div className={"col-12"}>
-                        <img src={carImage2} alt="Car image" />
-                    </div>
-                </div>
-                <div className={"row"}>
-                    <div className={"col-12 card-info-2"}>
-                        <div className={"row"}>
-                            <span className={"h3"} >{car.assembler} {car.model}</span>
-                        </div>
-                        <div className={"row"}>
-                            <span>{car.description}</span>
-                        </div>
-                        <div className={"row"}>
-                            <span>{car.year}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>*/
     )
 }
 
