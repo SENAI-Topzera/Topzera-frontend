38862420f08d3f95258db978b39df56f7727e49a
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-05-03 19:26:40 -0300

    filterbar from filter page finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,14 @@ import axios from 'axios';
 import { BASE_URL } from '../../utils/requests';
 import { Car } from '../../types/car';
 import CarCard from 'components/CarCard';
-import { Container, Row, Col, Nav, Form, Placeholder } from "react-bootstrap";
+import FilterBar from 'components/Filterbar';
+import { Container, Row, Col } from "react-bootstrap";
 //import './styles.css';
 
 function Home() {
 
     const [cars, setCars] = useState<Car[]>([]);
-    const [isFocus, setIsFocus] = useState(false);
-
+    
     useEffect(() => {
         axios.get(`${BASE_URL}/cars`)
             .then(response => {
@@ -19,50 +19,11 @@ function Home() {
             });
     }, []);
 
-    const changeToDateType = (event: React.FocusEvent<HTMLInputElement>) => {
-        setIsFocus(true);
-        console.log(event);
-    };
-
     return (
         <>
             <Row>
                 <Col md="3">
-                    <Nav defaultActiveKey="/home" className="flex-column bg-light vh-100">
-                        <Form.Group className="mb-3 mx-3 mt-3" controlId="formBasicPassword">
-                            <Form.Label>Localização</Form.Label>
-                            <Form.Control type="text" placeholder="Digite seu estado ou cidade" />
-                        </Form.Group>
-                        <Form.Group className="mb-3 mx-3" controlId="formBasicPassword">
-                            <Form.Label>Data de disponibilidade</Form.Label>
-                            <Row>
-                                <Col xs="6" className="px-0">
-                                    <Form.Control type="text" placeholder='Data Inicial' onFocus={changeToDateType}/>
-                                </Col>
-                                <Col xs="6">
-                                    <Form.Control type="date" placeholder="Data Final" />
-                                </Col>
-                            </Row>
-                        </Form.Group>
-                        <Form.Group className="mb-3 mx-3">
-                            <Form.Label>Tipo</Form.Label>
-                            <Form.Select>
-                            <option selected disabled>Selecione um tipo de carro</option>
-                                <option>Sedan</option>
-                                <option>SUV</option>
-                                <option>Hatch</option>
-                            </Form.Select>
-                        </Form.Group>
-                        <Form.Group className="mb-3 mx-3">
-                            <Form.Label>Quantidade de lugares</Form.Label>
-                            <Form.Select>
-                                <option selected disabled>Selecione uma quantidade</option>
-                                <option>2</option>
-                                <option>4</option>
-                                <option>7</option>
-                            </Form.Select>
-                        </Form.Group>
-                    </Nav>
+                    <FilterBar />
                 </Col>
                 <Col md="9">
                     <Container className='my-4'>
