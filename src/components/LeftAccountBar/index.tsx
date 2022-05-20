b275945761bbb553834b4f027430d3456206da0f
Jefferson <jefmaylon@gmail.com>
2022-05-20 19:47:13 -0300

    MyAccount & leftAccountBar

---
+++ src/components/LeftAccountBar/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import './styles.css';
+import { Container, Row, Col, Card, Button, Nav } from "react-bootstrap";
+
+class leftAccountBar extends React.Component {
+    render() {
+
+        return (
+            <Nav defaultActiveKey="/home" className="flex-column bg-light" id="container-side-bar">
+                <Row>
+                    <Col md="1"></Col>
+                    <Col md="10">
+                        <Container>
+                            <Card className='cardimg'>
+                            </Card>
+                            <Row>
+                                <Col>
+                                    <h6 className='text-center'>Godofredo da Silva</h6>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                    <Col md="1"></Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Container>
+                            <Row className="d-flex align-items-center justify-content-center flex-column">
+                                <Col md="1"></Col>
+                                <Col md="10">
+                                    <Button variant="primary" className='w-100'>
+                                        Minhas informações
+                                    </Button>
+                                </Col>
+                                <Col md="1"></Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Container>
+                            <Row className="d-flex align-items-center justify-content-center flex-column">
+                                <Col md="1"></Col>
+                                <Col md="10">
+                                    <Button variant="primary" className='w-100'>
+                                        Meus carros
+                                    </Button>
+                                </Col>
+                                <Col md="1"></Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Container>
+                            <Row className="d-flex align-items-center justify-content-center flex-column">
+                                <Col md="1"></Col>
+                                <Col md="10">
+                                    <Button variant="primary" className='w-100'>
+                                        Minha locação
+                                    </Button>
+                                </Col>
+                                <Col md="1"></Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col md="1"></Col>
+                                <Col md="10">
+                                    <Button variant="primary" className='w-100'>
+                                        Sair
+                                    </Button>
+                                </Col>
+                                <Col md="1"></Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+            </Nav>
+        )
+    }
+}
+
+export default leftAccountBar;
