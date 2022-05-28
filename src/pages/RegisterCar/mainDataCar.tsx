f89b7d4e708e12b885852d4cc5e3ec6084a54aa8
Igor Ponchielli <igor.ponchielli@gmail.com>
2022-05-28 15:12:55 -0300

    form completed

--- src/pages/RegisterCar/mainDataCar.tsx
+++ src/pages/RegisterCar/mainDataCar.tsx
@@ -15,12 +15,15 @@ function CarMainData() {
     const [idMarca, setMarca] = useState('');
     const [idModelo, setModelo] = useState('');
     const [ano, setAno] = useState('');
+    const [cor, setCor] = useState('');
+    const [placa, setPlaca] = useState('');
     const [combustivel, setCombustivel] = useState('');
+    const [cambio, setCambio] = useState('');
 
     const RenderAno = () => {
-        try {      
+        try {
             return (yearsCar?.map((year, index) => (
-                <option key={index} value={year.Value}>{year.Label}</option>
+                <option key={index} value={year.Value}>{year.Label.slice(0, 4)}</option>
             )))
         } catch (error) {
         }
@@ -80,19 +83,6 @@ function CarMainData() {
 
     }, [idModelo]);
 
-    useEffect(() => {
-        setCombustivel(ano.slice(5, 100));
-    }, [ano])
-
-    //{
-    //    const data = response.data as ModelCar[];
-    //   setModels(data);
-    //}
-
-    // useEffect(() => {
-    //     console.log('executa ao iniciar');
-    // }, []);
-
     return (
         <>
             {/* DADOS VEICULO */}
@@ -138,27 +128,44 @@ function CarMainData() {
                         <Col md="3">
                             <Form.Group className="mb-3" controlId="">
                                 <Form.Label>Cor</Form.Label>
-                                <Form.Select aria-label="Cor">
+                                <Form.Select aria-label="Cor" value={cor} onChange={e => setCor(e.target.value)}>
                                     <option disabled >Selecione</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                    <option value="1">AMARELO</option>
+                                    <option value="2">AZUL</option>
+                                    <option value="3">BEGE</option>
+                                    <option value="4">BRANCA</option>
+                                    <option value="5">CINZA</option>
+                                    <option value="6">DOURADA</option>
+                                    <option value="7">GRENA</option>
+                                    <option value="8">LARANJA</option>
+                                    <option value="9">MARROM</option>
+                                    <option value="10">PRATA</option>
+                                    <option value="11">PRETA</option>
+                                    <option value="12">ROSA</option>
+                                    <option value="13">ROXA</option>
+                                    <option value="14">VERDE</option>
+                                    <option value="15">VERMELHA</option>
+                                    <option value="16">FANTASIA</option>
                                 </Form.Select>
                             </Form.Group>
                         </Col>
                         <Col md="3">
                             <Form.Group className="mb-3" controlId="">
                                 <Form.Label>Placa</Form.Label>
-                                <Form.Control type="text" placeholder="" />
+                                <Form.Control type="text" placeholder="" value={placa} onChange={e => setPlaca(e.target.value)} />
                             </Form.Group>
                         </Col>
                         <Col md="3">
                             <Form.Group className="mb-3" controlId="">
                                 <Form.Label>Combustível usado</Form.Label>
-                                <Form.Select aria-label="Combustível usado">
+                                <Form.Select aria-label="Combustível usado" value={combustivel} onChange={e => setCombustivel(e.target.value)}>
                                     <option disabled >Selecione</option>
                                     <option value="1">GNV</option>
-                                    <option value="2">{combustivel}</option>
+                                    <option value="2">Álcool</option>
+                                    <option value="3">Diesel</option>
+                                    <option value="4">Elétrico</option>
+                                    <option value="5">Flex</option>
+                                    <option value="6">Gasolina</option>
                                 </Form.Select>
                             </Form.Group>
                         </Col>
@@ -188,13 +195,8 @@ function CarMainData() {
                         </Col>
                         <Col md="3">
                             <Form.Group className="mb-3" controlId="">
-                                <Form.Label>Capacidade de Bagagem</Form.Label>
-                                <Form.Select aria-label="Capacidade de Bagagem">
-                                    <option disabled >Selecione</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </Form.Select>
+                                <Form.Label>Capacidade de Bagagem (Litros)</Form.Label>
+                                <Form.Control type="number" min="1" placeholder="" />
                             </Form.Group>
                         </Col>
                         <Col md="3">
